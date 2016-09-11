97853da4cc220e1725d75ddb9940a93712e78140
Tompee <tompee26@yahoo.com>
2016-09-11 12:21:16 +0900

    Initial commit

---
+++ app/src/main/java/com/tompee/utilities/knowyourmeds/view/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tompee.utilities.knowyourmeds.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tompee.utilities.knowyourmeds.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
