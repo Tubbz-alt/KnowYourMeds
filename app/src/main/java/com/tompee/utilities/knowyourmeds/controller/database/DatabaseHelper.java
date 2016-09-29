1b2e67db6acc54d6372c4bb47156fc013c625a38
tompee <tompee@alink-group.co.jp>
2016-09-29 18:57:19 +0900

    Fixed crash when lists are empty

--- app/src/main/java/com/tompee/utilities/knowyourmeds/controller/database/DatabaseHelper.java
+++ app/src/main/java/com/tompee/utilities/knowyourmeds/controller/database/DatabaseHelper.java
@@ -187,7 +187,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 list.add(array.optString(index));
             }
             return list;
-        } catch (JSONException e) {
+        } catch (JSONException | NullPointerException e) {
             e.printStackTrace();
         }
         return null;
